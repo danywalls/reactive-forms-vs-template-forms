a7955b40b2a8f7abd7b81fa38a028695e55d6ce3
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-02-28 17:20:52 +0100

    feat(flights components): create two new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FlightsReactiveComponent } from './flights-reactive/flights-reactive.component';
+import { FlightsComponent } from './flights/flights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightsReactiveComponent,
+    FlightsComponent
   ],
   imports: [
     BrowserModule
